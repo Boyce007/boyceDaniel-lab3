4f5d917c4d3800bb4e21bea09c64517f448ad6b9
Boyce007 <diboyce21@students.desu.edu>
2023-11-30 15:13:01 +0000

    Updates

--- lab3.c
+++ lab3.c
@@ -9,44 +9,57 @@ int *worker_validation;
 
 int **read_board_from_file(char *filename)
 {
+    // printf("Before open");
     FILE *fp = fopen(filename, "r");
-    
-    int **test_board = (int**)malloc(ROW_SIZE * sizeof(int*));
+    // printf("%s \n","after open");
+    test_board = (int**)malloc(ROW_SIZE * sizeof(int*));
+
     for(int i= 0;i<ROW_SIZE;i++) {
+        // printf("i:%d \n",i);
         test_board[i] =(int*) malloc(COL_SIZE*sizeof(int));
     }
     if (fp == NULL){
         fprintf(stderr, "No file is found");
-        return;
-    }   
-        for(int i = 0;i<ROW_SIZE;i++) {
-            for (int j = 0;j<COL_SIZE;j++) {
-                fscanf(fp,"%d,",&test_board[i][j]);
-            }
+        exit(1);
+    }
+    // printf("%s","before read");   
+    for(int i = 0;i<ROW_SIZE;i++) {
+        for (int j = 0;j<COL_SIZE;j++) {
+            // printf("i:%d,j:%d",i,j);
+            fscanf(fp,"%d,",&(test_board[i][j]));
+            
         }
+    }
         fclose(fp);
+
     return test_board;
 }
 
 
 
 void* validate(void* p) {
-    printf("%s","Hello world");
+    
     param_struct* param = (param_struct*) p;
+    
     int validate_arr[9] = {0,0,0,0,0,0,0,0,0};
+    printf("in Validate ");
+    printf("begrow: %d endRow%d begCol%d endCol%d \n",param->starting_row,param->ending_row,param->starting_col,param->ending_col);
     for(int i =param->starting_row ;i<param->ending_row;i++) {
-        for (int j = param->starting_col;i<param->ending_row;i++) {
+        for (int j = param->starting_col;j<param->ending_col;j++) {
             int current_row_val = test_board[i][j];
+            printf("test board val");
+            printf("row:%d val:%d \n ",i,current_row_val);
             if(validate_arr[current_row_val-1]==1) { 
                 worker_validation[param->id] = 0; 
-                return;
-            }
-            validate_arr[current_row_val-1] = 1;
+                return NULL;
+            } else {
+                validate_arr[current_row_val-1] = 1;
+            } 
         }
     }
-    
     worker_validation[param->id] = 1;
 }
+
 
 
 
@@ -55,71 +68,99 @@ void* validate(void* p) {
 int is_board_valid() {
     
     pthread_t* tid = (pthread_t*) malloc(sizeof(int)*NUM_OF_THREADS);
-    
     pthread_attr_t attr;
-    
     param_struct* params = (param_struct*)malloc(sizeof(param_struct)*NUM_OF_THREADS);
     worker_validation = (int*) malloc(sizeof(int)*NUM_OF_THREADS);
+    int i =0;
     
-    for(int i= 0;i<ROW_SIZE;i++) {
+    
+    for(int row= 0;row<ROW_SIZE;row++) {
         
         params[i].id = i;
-        
-        params[i].starting_row = i;
+        params[i].starting_row = row;
         params[i].starting_col = 0;
-        params[i].ending_row = i;
-        params[i].ending_col = COL_SIZE-1;
+        params[i].ending_row = row;
+        params[i].ending_col = COL_SIZE;
+        
         pthread_create(&(tid[i]), &attr, validate, &(params[i]));
+        
+    
+        i++;
+
     }
 
 
-    for(int i= ROW_SIZE;i<ROW_SIZE+COL_SIZE;i++) {
-        
+    for(int col= 0;col<COL_SIZE;col++) {
         params[i].id = i;
-        
         params[i].starting_row = 0;
-        params[i].starting_col = i-ROW_SIZE;
-        params[i].ending_row = ROW_SIZE-1;
-        params[i].ending_col = i-ROW_SIZE;
+        params[i].starting_col = col;
+        params[i].ending_row = ROW_SIZE;
+        params[i].ending_col = col;
+        validate(&(params[i]));
+        
         pthread_create(&(tid[i]), &attr, validate, &(params[i]));
-    }
+        
+        i++;
 
-    for(int i =ROW_SIZE+COL_SIZE;i< NUM_OF_THREADS;i++ ) {
-        params[i].id = i;
-        for(int i =0;i<)
-        int bounds = i - (ROW_SIZE+COL_SIZE);
-        params[i].starting_row = bounds;
-        params[i].starting_col = bounds;
-        params[i].ending_row = bounds+2;
-        params[i].ending_col = bounds+2;
+    }
+    for(int row = 0; row<ROW_SIZE;row+=3) {
+        for(int col =0;col<COL_SIZE;col+=3) {
+            params[i].id = i;
+            params[i].starting_row = row;
+            params[i].starting_col = col;
+            params[i].ending_row = row+2 ;
+            params[i].ending_col = col +2;
+            
+            pthread_create(&(tid[i]), &attr, validate, &(params[i]));
+            
+            i++;
+
+        }
+        
+    }
+   
+    
+        
+
+
+        
+        
+    
     
 
-        pthread_create(&(tid[i]), &attr, validate, &(params[i]));
+    for(int i =0;i<NUM_OF_THREADS;i++) {
+        if(i<14) {
+            break;
+        }
+        
+        pthread_join(tid[i],NULL);
         
     }
     for(int i =0;i<NUM_OF_THREADS;i++) {
-        printf("thread id:%d isvalid %d",worker_validation[params[i].id],worker_validation[i]);
-    }
-    
+        // printf("i:%d val:%d \n",i,worker_validation[i]);
 
-    for(int i =0;i<NUM_OF_THREADS;i++) {
-        pthread_join(tid[i],NULL);
-        if(worker_validation[i]!=1) {
-            return 0;
-        }
     }
-    free(tid);
-    free(params);
-    free(worker_validation);
+        
+    
     return 1;
 }
 
 int main(int argc, char *argv[]){
+    
+    
     int** board = read_board_from_file(argv[1]);
-    printf("%d",is_board_valid());
     
     
+    int val = is_board_valid();
+    // for(int i =0;i<ROW_SIZE;i++){
+    //     for(int j =0;j<COL_SIZE;j++) {
+    //         printf("%d,",board[i][j]);
+    //     }
+    //     printf("\n");
 
+    // }    
+    
+    
 
 }
 
